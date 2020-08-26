fd471800780bb5f594d551b11d35392c4c863791
Pankaj <pankaj.rai16@gmail.com>
2020-08-26 13:27:17 +0530

    initial commit

---
+++ app/src/main/java/com/sample/motionlayoutfragment/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.sample.motionlayoutfragment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.constraintlayout.utils.widget.ImageFilterButton
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<ImageFilterButton>(R.id.ifbThumbsUp)
+        
+        supportFragmentManager.beginTransaction()
+            .add(R.id.flProfile, ProfileFragment())
+            .commit()
+    }
+}
---
+++ app/src/main/java/com/sample/motionlayoutfragment/ProfileFragment.kt
@@ -0,0 +1,11 @@
+package com.sample.motionlayoutfragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+
+class ProfileFragment : Fragment(R.layout.fragment_profile) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
